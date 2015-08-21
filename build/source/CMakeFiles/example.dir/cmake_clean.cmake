cf200dd63feb8c0c68046f337fc1a52389daa5c2
Leanhope <hans.lienhop@gmail.com>
2015-08-21 15:28:56 +0200

    debugging

--- build/source/CMakeFiles/example.dir/cmake_clean.cmake
+++ build/source/CMakeFiles/example.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/example.dir/example.cpp.o"
   "../build/Release/example.pdb"
   "../build/Release/example"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
