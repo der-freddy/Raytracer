bb8c2f3cc753700aaa61ade0e911a64e91a3e776
Frederik Sukop <frederik.sukop@uni-weiamr.de>
2015-08-27 12:18:54 +0200

    light fixed

--- build/framework/CMakeFiles/framework.dir/cmake_clean.cmake
+++ build/framework/CMakeFiles/framework.dir/cmake_clean.cmake
@@ -1,26 +1,26 @@
-FILE(REMOVE_RECURSE
+file(REMOVE_RECURSE
   "CMakeFiles/framework.dir/glew.c.o"
-  "CMakeFiles/framework.dir/sdfloader.cpp.o"
-  "CMakeFiles/framework.dir/cylinder.cpp.o"
-  "CMakeFiles/framework.dir/window.cpp.o"
   "CMakeFiles/framework.dir/pixel.cpp.o"
-  "CMakeFiles/framework.dir/ppmwriter.cpp.o"
-  "CMakeFiles/framework.dir/hit.cpp.o"
-  "CMakeFiles/framework.dir/cone.cpp.o"
-  "CMakeFiles/framework.dir/scene.cpp.o"
-  "CMakeFiles/framework.dir/sphere.cpp.o"
-  "CMakeFiles/framework.dir/composite.cpp.o"
-  "CMakeFiles/framework.dir/camera.cpp.o"
+  "CMakeFiles/framework.dir/window.cpp.o"
+  "CMakeFiles/framework.dir/cylinder.cpp.o"
   "CMakeFiles/framework.dir/light.cpp.o"
-  "CMakeFiles/framework.dir/material.cpp.o"
   "CMakeFiles/framework.dir/shape.cpp.o"
+  "CMakeFiles/framework.dir/camera.cpp.o"
+  "CMakeFiles/framework.dir/ppmwriter.cpp.o"
+  "CMakeFiles/framework.dir/scene.cpp.o"
   "CMakeFiles/framework.dir/box.cpp.o"
+  "CMakeFiles/framework.dir/sdfloader.cpp.o"
+  "CMakeFiles/framework.dir/material.cpp.o"
+  "CMakeFiles/framework.dir/sphere.cpp.o"
+  "CMakeFiles/framework.dir/hit.cpp.o"
   "CMakeFiles/framework.dir/renderer.cpp.o"
+  "CMakeFiles/framework.dir/composite.cpp.o"
+  "CMakeFiles/framework.dir/cone.cpp.o"
   "libframework.pdb"
   "libframework.a"
 )
 
 # Per-language clean rules from dependency scanning.
-FOREACH(lang C CXX)
-  INCLUDE(CMakeFiles/framework.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
+foreach(lang C CXX)
+  include(CMakeFiles/framework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
