5b1909d155a6ff1af9fabf29f7619eb9ec6488bc
Frederik Sukop <frederik.sukop@uni-weiamr.de>
2015-08-27 15:03:08 +0200

    diffuse light bugfixing

--- build/source/CMakeFiles/example.dir/DependInfo.cmake
+++ build/source/CMakeFiles/example.dir/DependInfo.cmake
@@ -1,34 +1,34 @@
 # The set of languages for which implicit dependencies are needed:
-SET(CMAKE_DEPENDS_LANGUAGES
+set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/hans/Desktop/Raytracer/source/example.cpp" "/home/hans/Desktop/Raytracer/build/source/CMakeFiles/example.dir/example.cpp.o"
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/source/example.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/source/CMakeFiles/example.dir/example.cpp.o"
   )
-SET(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
-SET(CMAKE_TARGET_DEFINITIONS
-  "FENSTERCHEN_BINARY_DIR=\"/home/hans/Desktop/Raytracer/build\""
-  "FENSTERCHEN_SOURCE_DIR=\"/home/hans/Desktop/Raytracer\""
+set(CMAKE_TARGET_DEFINITIONS
+  "FENSTERCHEN_BINARY_DIR=\"/home/freddy/Dokumente/Studium/Prog/Raytracer/build\""
+  "FENSTERCHEN_SOURCE_DIR=\"/home/freddy/Dokumente/Studium/Prog/Raytracer\""
   "GLEW_STATIC"
   )
 
 # Targets to which this target links.
-SET(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/DependInfo.cmake"
-  "/home/hans/Desktop/Raytracer/build/external/glfw-3.0.3/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/DependInfo.cmake"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/external/glfw-3.0.3/src/CMakeFiles/glfw.dir/DependInfo.cmake"
   )
 
 # The include file search paths:
-SET(CMAKE_C_TARGET_INCLUDE_PATH
+set(CMAKE_C_TARGET_INCLUDE_PATH
   "../external"
   "../external/glfw-3.0.3/include"
   "../external/glm-0.9.5.3"
   "../external/catch-1.1"
   "../framework"
   )
-SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
