f7498febd49455d7f50500604aacf44b75b91bc1
Frederik Sukop <frederik.sukop@uni-weiamr.de>
2015-08-28 13:11:23 +0200

    spec light

--- build/framework/CMakeFiles/framework.dir/DependInfo.cmake
+++ build/framework/CMakeFiles/framework.dir/DependInfo.cmake
@@ -1,46 +1,46 @@
 # The set of languages for which implicit dependencies are needed:
-SET(CMAKE_DEPENDS_LANGUAGES
+set(CMAKE_DEPENDS_LANGUAGES
   "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-SET(CMAKE_DEPENDS_CHECK_C
-  "/home/hans/Desktop/Raytracer/framework/glew.c" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/glew.c.o"
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/glew.c" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/glew.c.o"
   )
-SET(CMAKE_C_COMPILER_ID "GNU")
-SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/hans/Desktop/Raytracer/framework/box.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/box.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/camera.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/camera.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/composite.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/composite.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/cone.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/cone.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/cylinder.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/cylinder.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/hit.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/hit.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/light.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/light.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/material.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/material.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/pixel.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/pixel.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/ppmwriter.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/ppmwriter.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/renderer.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/renderer.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/scene.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/scene.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/sdfloader.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/sdfloader.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/shape.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/shape.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/sphere.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/sphere.cpp.o"
-  "/home/hans/Desktop/Raytracer/framework/window.cpp" "/home/hans/Desktop/Raytracer/build/framework/CMakeFiles/framework.dir/window.cpp.o"
+set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/box.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/box.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/camera.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/camera.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/composite.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/composite.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/cone.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/cone.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/cylinder.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/cylinder.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/hit.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/hit.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/light.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/light.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/material.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/material.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/pixel.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/pixel.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/ppmwriter.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/ppmwriter.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/renderer.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/renderer.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/scene.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/scene.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/sdfloader.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/sdfloader.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/shape.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/shape.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/sphere.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/sphere.cpp.o"
+  "/home/freddy/Dokumente/Studium/Prog/Raytracer/framework/window.cpp" "/home/freddy/Dokumente/Studium/Prog/Raytracer/build/framework/CMakeFiles/framework.dir/window.cpp.o"
   )
-SET(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
-SET(CMAKE_TARGET_DEFINITIONS
-  "FENSTERCHEN_BINARY_DIR=\"/home/hans/Desktop/Raytracer/build\""
-  "FENSTERCHEN_SOURCE_DIR=\"/home/hans/Desktop/Raytracer\""
+set(CMAKE_TARGET_DEFINITIONS
+  "FENSTERCHEN_BINARY_DIR=\"/home/freddy/Dokumente/Studium/Prog/Raytracer/build\""
+  "FENSTERCHEN_SOURCE_DIR=\"/home/freddy/Dokumente/Studium/Prog/Raytracer\""
   "GLEW_STATIC"
   )
 
 # Targets to which this target links.
-SET(CMAKE_TARGET_LINKED_INFO_FILES
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # The include file search paths:
-SET(CMAKE_C_TARGET_INCLUDE_PATH
+set(CMAKE_C_TARGET_INCLUDE_PATH
   "../external"
   "../external/glfw-3.0.3/include"
   "../external/glm-0.9.5.3"
@@ -48,6 +48,6 @@ SET(CMAKE_C_TARGET_INCLUDE_PATH
   "../framework"
   "../framework/.."
   )
-SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
